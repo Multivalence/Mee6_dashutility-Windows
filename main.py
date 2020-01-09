f6394c9eb6d62077374ea877079af31ff0053d0e
Multivalence <47169252+Multivalence@users.noreply.github.com>
2020-01-08 19:20:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#REMOVING IN BULK COMING SOON
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import StaleElementReferenceException
 import time
 
 print("\nThis is only Designed for Windows and requires Chrome\n")
@@ -18,10 +18,13 @@ browser.get("https://google.com")
 while True:
     selection = input('''\n\nPlease go to mee6.xyz on the new chrome tab that has opened. 
     Go to the moderator plugin and select the input bar for bad words. 
-    A cursor should show. After that is done, 
-    if you wish to remove in bulk, press r then enter.
-    If you wish to add in bulk, press a then enter : ''')
+    A cursor should show. After that is done,
 
+    - If you wish to remove in bulk, press "r" then enter.
+    - If you wish to add in bulk, press "a" then enter
+    - If you wish to exit the program, press "e" then enter : ''')
+    
+    #ADDING ALL BAD WORDS
     if selection == 'a':
 
         bar = browser.find_element_by_class_name('TagAddInput')
@@ -35,10 +38,29 @@ while True:
             bar.send_keys(x[i])
             time.sleep(0.1)
             bar.send_keys(Keys.ENTER)
-        break
-
+            
+        
+    #REMOVING ALL BAD WORDS
     elif selection == 'r':
-        print("Removing in Bulk coming soon")
+        xmarks = browser.find_elements_by_class_name('TagCross')
+
+        while True:
+            try:
+                if len(xmarks) == 0:
+                    break
+
+
+                for i in xmarks:
+                    i.click()
+                    time.sleep(0.1)
+
+            except StaleElementReferenceException:
+                print('StaleElementReferenceException while trying to click element. trying to find element again.' )
+                xmarks = browser.find_elements_by_class_name('TagCross')
+                
+    
+    #EXITING
+    elif selection == 'e':
         break
 
     else:
